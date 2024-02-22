079440fdbad83e454b336be0c7ae8c954891274a
Herison Pereira <herisonup@gmail.com>
2024-02-21 22:03:51 -0300

    feat: create container component

---
+++ src/components/ui/container/container.ts
@@ -0,0 +1,12 @@
+'use client';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	max-width: 1280px;
+	padding-inline: 2rem;
+	margin-inline: auto;
+
+	@media (max-width: 768px) {
+		padding-inline: 1rem;
+	}
+`;
